7861d35f7223e0a4c43a6ad5240bdd5a56b4fe3a
Herman <hrivas5726@aol.com>
2019-01-14 01:54:31 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,12 +3,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  10 - 5
 end
 
 def division(num1, num2)
+  50/2
 end
 
 def multiplication(num1, num2)
+  4*30
 end
 
 def modulo(num1, num2)
